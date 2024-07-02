5853cd825f4e209cd3f8d5583fbe68d4643be74b
Kevin Cearns <kevin.cearns@rewardops.com>
2024-07-02 14:27:52 -0400

    argo logs

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Gandalf!');
+  res.send('Hello ArgoCD!');
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port} in the Shire!`);
+  console.log(`App listening at http://localhost:${port} in the ArogCD!`);
 });
